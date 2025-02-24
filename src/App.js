160998b4b1514f420d9f9f19605027086ba7af66
Miguel <miguelangelcaprin@outlook.com>
2025-02-23 21:47:27 -0300

    4 cambios

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 // import { useState } from "react";
 // import { CartProvider } from "./context/CartContext";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import CartWidget from "./components/CartWidget";
+// import NavBar from "./components/NavBar";  // Importa el NavBar
 // import ItemListContainer from "./components/ItemListContainer";
 // import Cart from "./components/Cart"; 
 // import Checkout from "./components/Checkout";
 // import Loader from "./components/Loader"; // Importamos el Loader
 // import './css/App.css';
-
+// import { useCart } from "./context/CartContext";  // Usamos el hook del carrito
 
 // function App() {
 //   const [loading, setLoading] = useState(true);
+//   const { carrito } = useCart();  // Obtenemos el carrito desde el contexto
 
 //   return (
 //     <CartProvider>
@@ -20,9 +21,7 @@
 //           <Loader onFinish={() => setLoading(false)} />
 //         ) : (
 //           <div>
- 
-//             <h1>¡Bienvenido a mi E-commerce!</h1>
-//             <CartWidget />
+//             <NavBar carrito={carrito} /> {/* Incluimos el NavBar */}
 //             <Routes>
 //               <Route path="/" element={<ItemListContainer />} />
 //               <Route path="/cart" element={<Cart />} />
@@ -36,6 +35,8 @@
 // }
 
 // export default App;
+
+
 import { useState } from "react";
 import { CartProvider } from "./context/CartContext";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -44,6 +45,7 @@ import ItemListContainer from "./components/ItemListContainer";
 import Cart from "./components/Cart"; 
 import Checkout from "./components/Checkout";
 import Loader from "./components/Loader"; // Importamos el Loader
+import Footer from './components/Footer';  // Importa el Footer
 import './css/App.css';
 import { useCart } from "./context/CartContext";  // Usamos el hook del carrito
 
@@ -64,6 +66,7 @@ function App() {
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
             </Routes>
+            <Footer />  {/* Agregamos el Footer aquí */}
           </div>
         )}
       </Router>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../css/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2025 Old School Classics. Todos los derechos reservados.</p>
+        <div className="footer-links">
+          <a href="/about" className="footer-link">Sobre Nosotros</a>
+          <a href="/contact" className="footer-link">Contacto</a>
+          <a href="/terms" className="footer-link">Términos de Servicio</a>
+        </div>
+        <div className="footer-social">
+          <a href="https://facebook.com" target='blank' className="social-icon">
+            <img src="/facebook.png" alt="Facebook"  className="social-icon-img" />
+          </a>
+          <a href="https://twitter.com" target='blank' className="social-icon">
+            <img src="/twiter.png" alt="Twitter" className="social-icon-img" />
+          </a>
+          <a href="https://instagram.com" target='blank' className="social-icon">
+            <img src="/instagram.png" alt="Instagram" className="social-icon-img" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
